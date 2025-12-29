d844c2c2e4da9dc107b9d593830a913964d1a053
Krisna Wira Indrawan <krisnawiraindrawan@gmail.com>
2025-12-29 20:45:31 +0700

    bit of improvement on sidebar ui

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,17 +10,17 @@ export const Navbar = () => {
   };
 
   return (
-    <div className="flex flex-col gap-6 w-3xs h-screen p-4 ">
+    <div className="flex flex-col gap-6 w-3xs h-screen p-4">
       <div className="flex flex-row items-center justify-between">
         <h1 id="title" className="text-3xl font-bold text-accent">
           Perofin
         </h1>
-        <p id="versions" className="text-base">
+        <p id="versions" className="text-base font-bold">
           v1.0
         </p>
       </div>
 
-      <div className="flex flex-col gap-0.5">
+      <div className="flex flex-col gap-1.5">
         <p className="font-semibold text-sm text-gray-900 opacity-70">
           GENERAL
         </p>
@@ -28,9 +28,9 @@ export const Navbar = () => {
           <li
             onClick={() => handleNav("dashboard")}
             className={clsx(
-              "cursor-pointer p-2 flex flex-row gap-4",
+              "cursor-pointer p-3 flex flex-row gap-4",
               active === "dashboard"
-                ? "font-bold bg-accent rounded-2xl text-textAlt"
+                ? "font-bold bg-accent-select rounded-2xl text-accent"
                 : ""
             )}
           >
@@ -40,9 +40,9 @@ export const Navbar = () => {
           <li
             onClick={() => handleNav("transaction")}
             className={clsx(
-              "cursor-pointer p-2 flex flex-row gap-4",
+              "cursor-pointer p-3 flex flex-row gap-4",
               active === "transaction"
-                ? "font-bold bg-accent rounded-2xl text-textAlt"
+                ? "font-bold bg-accent-select rounded-2xl text-accent"
                 : ""
             )}
           >
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+import { Navbar } from "../components/navbar";
+
+<div className="flex flex-row gap-2">
+  <Navbar />
+  <div id="dashboard-content" className="flex-1">
+    Content
+  </div>
+</div>;
